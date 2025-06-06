f668f82ff6127e535edcfcd4e407bdd0041c782e
seungdobae <svvwac98@saltware.co.kr>
2025-06-06 12:38:27 +0900

    fix: thanos config 수치 조정

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_internet_gateway" "main" {
 # Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.112.1.0/24"
   availability_zone       = "ap-northeast-2a"
   map_public_ip_on_launch = true
 
