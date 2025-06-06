a3ea6fa48ee71aee8872d1c4358e5576ad7a28a5
seungdobae <svvwac98@saltware.co.kr>
2025-06-06 12:15:03 +0900

    fix: thanos-compactor pv 증설 8gib > 20gib

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.112.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
